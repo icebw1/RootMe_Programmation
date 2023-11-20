66fea132427a211e1e63a4e9eeedb2c412fd51a8
big mitch <bigmitch@air-de-big.home>
2023-11-20 23:00:47 +0100

    some changes

--- DecoderBase64.py
+++ DecoderBase64.py
@@ -1,11 +1,16 @@
 import base64
 
+
+
 chaineADecoder = "aHR0cHM6Ly90ZXh0YmVsdC5jb20vdGV4dA==" # string encodé en base 64
-chaine_dechiffree = base64.b64decode(chaineADecoder).decode('utf-8') # string décodé en ut8
-print("Chaine chiffrée :", chaineADecoder)
-print("Chaine déchiffrée :", chaine_dechiffree)
 
+try:
+    chaine_dechiffree = base64.b64decode(chaineADecoder).decode('utf-8') # string décodé en ut8
+    print("Chaine chiffrée :", chaineADecoder)
+    print("Chaine déchiffrée :", chaine_dechiffree)
 
+except:
+    print("Erreur lors du déchiffrage.")
 
 
 
@@ -14,12 +19,3 @@ print("Chaine déchiffrée :", chaine_dechiffree)
 
 # etape1 = base64.b64decode(chaineADecoder) # Pareil que : base64.b64decode(chaineADecoder.encode('ascii'))
 # etape2 = etape1.decode('ascii')
-
-# try:
-#     chaine_dechiffree = base64.b64decode(chaineADecoder).decode('utf-8')
-#     print("Chaine chiffrée :", chaineADecoder)
-#     print("Chaine déchiffrée :", chaine_dechiffree)
-#     # print("Message :", message)
-
-# except:
-#     print("Erreur lors du déchiffrage.")
